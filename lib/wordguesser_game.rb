b8ff15137510390940bd2f351f00e10522816efb
moyunxiang <2556377578@qq.com>
2026-07-03 23:42:17 -0700

    Complete CHIPS Wordguesser assignment

---
+++ lib/wordguesser_game.rb
@@ -0,0 +1,52 @@
+class WordGuesserGame
+
+  attr_accessor :word, :guesses, :wrong_guesses
+
+  def initialize(word)
+    @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  def guess(letter)
+    raise ArgumentError, 'guess must be a single letter' unless
+      letter.is_a?(String) && letter =~ /\A[A-Za-z]\z/
+    letter = letter.downcase
+    return false if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+    if @word.include?(letter)
+      @guesses += letter
+    else
+      @wrong_guesses += letter
+    end
+    true
+  end
+
+  def word_with_guesses
+    @word.chars.map { |char| @guesses.include?(char) ? char : '-' }.join
+  end
+
+  def check_win_or_lose
+    if word_with_guesses == @word
+      :win
+    elsif @wrong_guesses.length >= 7
+      :lose
+    else
+      :play
+    end
+  end
+
+  # Get a word from remote "random word" service
+
+  # You can test it by installing irb via $ gem install irb
+  # and then running $ irb -I. -r app.rb
+  # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
+  #  => "cooking"   <-- some random word
+  def self.get_random_word
+    require 'uri'
+    require 'net/http'
+    uri = URI('http://randomword.saasbook.info/RandomWord')
+    Net::HTTP.new('randomword.saasbook.info').start do |http|
+      return http.post(uri, "").body
+    end
+  end
+end
